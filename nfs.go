66b072b269b2c20d58f8623947100bb57b54fd20
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-08-03 20:43:01 -0800

    nfs: finish Readdir, make it work
    'make benchmark' passes now
    
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>
    Signed-off-by: Irbe Krumina <irbe@tailscale.com>

--- nfs.go
+++ nfs.go
@@ -12,6 +12,7 @@ import (
 	"net"
 	"os"
 	"path"
+	"path/filepath"
 	"slices"
 	"strings"
 	"sync"
@@ -81,10 +82,10 @@ type billyFile struct {
 
 func (f billyFile) Name() string            { panic("unused") } // only on create
 func (billyFile) Close() error              { return nil }
-func (billyFile) Lock() error               { return errReadonly }
-func (billyFile) Unlock() error             { return errReadonly }
 func (billyFile) Truncate(int64) error      { return errReadonly }
 func (billyFile) Write([]byte) (int, error) { return 0, errReadonly }
+func (billyFile) Lock() error               { return errReadonly }
+func (billyFile) Unlock() error             { return errReadonly }
 
 func newBillyFileFromBytes(data []byte) billy.File {
 	sr := io.NewSectionReader(bytes.NewReader(data), 0, int64(len(data)))
@@ -94,7 +95,7 @@ func newBillyFileFromBytes(data []byte) billy.File {
 }
 
 func (b billyFS) Open(filename string) (billy.File, error) {
-	log.Printf("gomodfs NFS Open called with %q", filename)
+	log.Printf("TSGO: gomodfs NFS Open called with %q", filename)
 	mp := parsePath(filename)
 	if mp.NotExist {
 		return nil, os.ErrNotExist
@@ -102,11 +103,13 @@ func (b billyFS) Open(filename string) (billy.File, error) {
 	switch mp.WellKnown {
 	case "":
 		// nothing
-	default:
-		return nil, fmt.Errorf("gomodfs TODO well known Open %q", filename)
 	case statusFile:
 		j := b.fs.statusJSON()
 		return newBillyFileFromBytes(j), nil
+	case "tsgo.extracted":
+		return newBillyFileFromBytes(nil), nil
+	default:
+		return nil, fmt.Errorf("gomodfs TODO well known Open %q", filename)
 	}
 
 	ctx := context.TODO()
@@ -189,7 +192,39 @@ func (b billyFS) ReadDir(path string) ([]os.FileInfo, error) {
 			dirFileInfo{baseName: "download"},
 		}, nil
 	}
-	return nil, nil
+
+	mp := parsePath(path)
+	if mp.NotExist {
+		return nil, os.ErrNotExist
+	}
+	if !mp.InZip {
+		return nil, nil
+	}
+
+	ctx := context.TODO()
+	mh, err := b.fs.getZipRoot(ctx, mp.ModVersion)
+	if err != nil {
+		return nil, err
+	}
+	spanRD := b.fs.Stats.StartSpan("nfs.Readdir")
+	ents, err := b.fs.Store.Readdir(ctx, mh, mp.Path)
+	spanRD.End(err)
+
+	ret := make([]os.FileInfo, len(ents))
+	for i, ent := range ents {
+		if ent.Mode.IsDir() {
+			ret[i] = dirFileInfo{baseName: ent.Name}
+			continue
+		}
+		ret[i] = regFileInfo{
+			name: ent.Name,
+			size: ent.Size,
+			mode: ent.Mode.Perm(),
+		}
+		// TODO: symlinks? but go modules and tsgo don't use them?
+	}
+
+	return ret, nil
 }
 
 func (b billyFS) Lstat(filename string) (os.FileInfo, error) {
@@ -201,9 +236,17 @@ func (b billyFS) Lstat(filename string) (os.FileInfo, error) {
 	case "":
 		// Nothing
 	case statusFile:
+		j := b.fs.statusJSON()
 		return regFileInfo{
 			name:       statusFile,
-			size:       100,
+			size:       int64(len(j)),
+			mode:       0444,
+			modTimeNow: true,
+		}, nil
+	case "tsgo.extracted":
+		return regFileInfo{
+			name:       "tsgo.extracted",
+			size:       0,
 			mode:       0444,
 			modTimeNow: true,
 		}, nil
@@ -212,9 +255,15 @@ func (b billyFS) Lstat(filename string) (os.FileInfo, error) {
 		return nil, errors.New("TODO")
 	}
 
-	if mp.CacheDownloadFileExt != "" {
-		log.Printf("NFS: TODO cache-download %v %v", mp.ModVersion, mp.CacheDownloadFileExt)
-		return nil, errors.New("TODO")
+	ctx := context.TODO()
+
+	if ext := mp.CacheDownloadFileExt; ext != "" {
+		v, err := b.fs.getMetaFileByExt(ctx, mp.ModVersion, ext)
+		if err != nil {
+			log.Printf("Failed to get %s file for %v: %v", ext, mp.ModVersion, err)
+			return nil, syscall.EIO
+		}
+		return regFileInfo{name: filepath.Base(filename), size: int64(len(v))}, nil
 	}
 
 	if !mp.InZip {
@@ -222,7 +271,6 @@ func (b billyFS) Lstat(filename string) (os.FileInfo, error) {
 		return dirFileInfo{}, nil
 	}
 
-	ctx := context.Background()
 	mh, err := b.fs.getZipRoot(ctx, mp.ModVersion)
 	if err != nil {
 		return nil, err
@@ -295,7 +343,6 @@ func (h *NFSHandler) ToHandle(fs billy.Filesystem, path []string) []byte {
 	}
 	handle := handle(ret)
 	if _, ok := h.handle[handle]; !ok {
-		log.Printf("ToHandle(%q) addded => %q", pathStr, ret)
 		h.handle[handle] = slices.Clone(path)
 	}
 	return ret
